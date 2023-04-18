af44f6c0ef231fd428087ffc17080d3e04ce381e
Bianca <124810209+biancabsfml@users.noreply.github.com>
2023-04-18 15:17:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const calcularBtn = document.querySelector('#calcular')
+calcularBtn.addEventListener('click', calculargorjeta)
+
+
+function calculargorjeta() {
+    const total = document.querySelector('#total').value
+    const nPessoas = document.querySelector('#nPessoas').value
+    const porcentagem = document.querySelector('#porcentagem').value
+
+if (!total || !nPessoas)  {
+
+        alert('Campos Vazios.')
+
+ }else{
+        const gorjeta = (total * (porcentagem / 100)) / nPessoas
+        const valorGorjeta = document.querySelector('#valor-gorjeta')
+        valorGorjeta.innerText = 'R$' + gorjeta.toFixed(2)   
+}
+
+}
+
+
